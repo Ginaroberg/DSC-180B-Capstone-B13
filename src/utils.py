a6ab3a8c00c7be467ec0555a4890969a5bc79a40
unknown <zou864276811@gmail.com>
2025-03-08 19:09:01 -0800

    update utils

--- src/utils.py
+++ src/utils.py
@@ -4,83 +4,102 @@ import pandas as pd
 from eofs.xarray import Eof
 
 
-input_dir = './processed_input/' # CONFIGURE ME
-output_dir = './processed_output/' # CONFIGURE ME
+input_dir = "./CMIP6_input/"
+output_dir = "./processed_output/"
+
+
+min_co2 = 0.
+max_co2 = 9500
+def normalize_co2(data):
+    return data / max_co2
+
+def un_normalize_co2(data):
+    return data * max_co2
+
+min_ch4 = 0.
+max_ch4 = 0.8
+def normalize_ch4(data):
+    return data / max_ch4
+
+def un_normalize_ch4(data):
+    return data * max_ch4
 
 
 
-def compute_eofs(X, var, n_eofs):
-    X_mean = X[var].mean(dim='time')
-    X_std = X[var].std(dim='time')
-    X_norm = (X[var] - X_mean) / X_std
-
-    solver = Eof(X_norm)
-    eofs = solver.eofsAsCorrelation(neofs=n_eofs)
-    pcs = solver.pcs(npcs=n_eofs, pcscaling=1)
-    
-    pcs_df = pcs.to_dataframe().unstack('mode')
-    pcs_df.columns = [f"{var}_{i}" for i in range(n_eofs)]
-    
-    return pcs_df, solver, X_mean, X_std
-
-
-
-
-
-def get_Xtrain(datasets, n_eofs=5):
-    
-    if isinstance(datasets, str):
-        datasets = [datasets]
+def get_Xtrain(data_sets, n_eofs=5):
+    """
+    Args:
+        data_sets list(str): names of datasets
+        n_eofs (int): number of eofs to create for aerosol variables
+    """
         
-    X = xr.concat([xr.open_dataset(input_dir + f'inputs_{d}.nc') for d in datasets], dim='time')
-    X = X.assign_coords(time=np.arange(len(X.time)))  # Reindex time
+    # Create training and testing arrays
+    if isinstance(data_sets, str):
+        data_sets = [data_sets]
+    X = xr.concat([xr.open_dataset(input_dir + f"inputs_{file}.nc") for file in data_sets], dim='time')
+    X = X.assign_coords(time=np.arange(len(X.time)))
 
-    variables = ['pr', 'rlds', 'rsds', 'sfcwind', 'tas', 'tasmax', 'tasmin']
+    # Compute EOFs for BC
+    bc_solver = Eof(X['BC'])
+    bc_eofs = bc_solver.eofsAsCorrelation(neofs=n_eofs)
+    bc_pcs = bc_solver.pcs(npcs=n_eofs, pcscaling=1)
 
-    results = [compute_eofs(X, var, n_eofs) for var in variables]
-    
-    dfs, solvers, means, stds = zip(*results)
-    
-    inputs = pd.concat(dfs, axis=1)
-    
-    mean_dict = dict(zip(variables, means))
-    std_dict = dict(zip(variables, stds))
-    
-    return inputs, solvers, mean_dict, std_dict
+    # Compute EOFs for SO2
+    so2_solver = Eof(X['SO2'])
+    so2_eofs = so2_solver.eofsAsCorrelation(neofs=n_eofs)
+    so2_pcs = so2_solver.pcs(npcs=n_eofs, pcscaling=1)
+
+    # Convert to pandas
+    bc_df = bc_pcs.to_dataframe().unstack('mode')
+    bc_df.columns = [f"BC_{i}" for i in range(n_eofs)]
+
+    so2_df = so2_pcs.to_dataframe().unstack('mode')
+    so2_df.columns = [f"SO2_{i}" for i in range(n_eofs)]
+
+    # Bring the emissions data back together again and normalise
+    inputs = pd.DataFrame({
+        "CO2": normalize_co2(X["CO2"].data),
+        "CH4": normalize_ch4(X["CH4"].data)
+    }, index=X["CO2"].coords['time'].data)
+
+    # Combine with aerosol EOFs
+    inputs = pd.concat([inputs, bc_df, so2_df], axis=1)
+    return inputs, (so2_solver, bc_solver)
 
 
 
+def get_Xtest(file, eof_solvers, n_eofs=5):
+    """
+    Args:
+        file str: name of datasets
+        n_eofs (int): number of eofs to create for aerosol variables
+        eof_solvers (Eof_so2, Eof_bc): Fitted Eof objects to use for projection
+    """
+        
+    # Create training and testing arrays
+    X = xr.open_dataset(input_dir + f"inputs_{file}.nc")
+        
+    so2_pcs = eof_solvers[0].projectField(X["SO2"], neofs=5, eofscaling=1)
+    so2_df = so2_pcs.to_dataframe().unstack('mode')
+    so2_df.columns = [f"SO2_{i}" for i in range(n_eofs)]
 
-def project_eofs(X, var, solver, mean, std, n_eofs):
-    X_norm = (X[var] - mean) / std
-    
-    pcs = solver.projectField(X_norm, neofs=n_eofs, eofscaling=1)
-    pcs_df = pcs.to_dataframe().unstack('mode')
-    pcs_df.columns = [f"{var}_{i}" for i in range(n_eofs)]
-    
-    return pcs_df
+    bc_pcs = eof_solvers[1].projectField(X["BC"], neofs=5, eofscaling=1)
+    bc_df = bc_pcs.to_dataframe().unstack('mode')
+    bc_df.columns = [f"BC_{i}" for i in range(n_eofs)]
 
+    # Bring the emissions data back together again and normalise
+    inputs = pd.DataFrame({
+        "CO2": normalize_co2(X["CO2"].data),
+        "CH4": normalize_ch4(X["CH4"].data)
+    }, index=X["CO2"].coords['time'].data)
 
-
-
-
-def get_Xtest(file, eof_solvers, mean_dict, std_dict, n_eofs=5):
-    X = xr.open_dataset(input_dir + f'inputs_{file}.nc')
-
-    variables = ['pr', 'rlds', 'rsds', 'sfcwind', 'tas', 'tasmax', 'tasmin']
-
-    dfs = [
-        project_eofs(X, var, solver, mean_dict[var], std_dict[var], n_eofs)
-        for var, solver in zip(variables, eof_solvers)
-    ]
-
-    return pd.concat(dfs, axis=1)
-
+    # Combine with aerosol EOFs
+    inputs = pd.concat([inputs, bc_df, so2_df], axis=1)
+    return inputs
 
 
 
 def get_Ytrain(datasets):
-
     if isinstance(datasets, str):
         datasets = [datasets]
         
